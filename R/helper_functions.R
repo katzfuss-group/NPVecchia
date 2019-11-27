c5c05fd719b5d25087144ee646ffc9e69f5c37bb
Brian Kidd <kidd6@tamu.edu>
2019-11-27 16:02:58 -0600

    Fixed typo in helper function documentation

--- R/helper_functions.R
+++ R/helper_functions.R
@@ -4,7 +4,7 @@
 #' there are many alternatives to mvrnorm in R, which isn't important for the package usefulness.
 #' Lastly, I was curious to see how internal functions work, as it is "good coding practice."
 #'
-#' @param covar_truecovariance matrix (must be symmetric and positive definite)
+#' @param covar_true covariance matrix (must be symmetric and positive definite)
 #' @param N number of replications
 #'
 #' @return an N * n matrix with each row corresponding to a replication of all covariates/locations
